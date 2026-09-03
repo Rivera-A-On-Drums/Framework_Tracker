7a837b188c4e85ec41b6da76be9b1e4159de3a01
Rivera-A-On-Drums <angelrivera0317@gmail.com>
2026-09-03 08:42:22 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'steady-v1';
+const CACHE_NAME = 'steady-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
+  '/audio_theme.mp3'
 ];
 
 self.addEventListener('install', (event) => {
